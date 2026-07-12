0aee3c339a15ffd8d38eec0a72b77232c9614372
socialboosthub <muxidinxamza@gmail.com>
2026-07-12 11:29:26 +0300

    Add receiveUpdates field to donation success response

--- api/donation-success.js
+++ api/donation-success.js
@@ -56,6 +56,7 @@ export default async function handler(req, res) {
       email,
       amount: Number(amount),
       mode,
+      receiveUpdates,
       paypalStatus: order.status,
       payer:
         order.payer?.email_address || email,
